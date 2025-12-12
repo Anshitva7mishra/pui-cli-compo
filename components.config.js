ccf487fa23f1c63f39b7bec157412d21253dd25e
Anshitva7Mishra <mishraanshitva@gmail.com>
2025-12-13 03:25:19 +0530

    go

---
+++ components.config.js
@@ -0,0 +1,41 @@
+const components = {
+  navbar: {
+    label: "Navbar",
+    items: {
+      glass_navbar: {
+        name: "Glass Navbar",
+        repo: "https://github.com/projectUI2k25/glass-navbar.git",
+        description: "Glassmorphism animated navbar",
+      },
+    },
+  },
+
+  sidebar: {
+    label: "Sidebar",
+    items: {
+      minimal_sidebar: {
+        name: "Minimal Sidebar",
+        repo: "https://github.com/projectUI2k25/minimal-sidebar.git",
+        description: "Clean minimal sidebar",
+      },
+    },
+  },
+
+  auth: {
+    label: "Auth",
+    items: {
+      pookie_auth: {
+        name: "Pookie Auth",
+        repo: "https://github.com/Anshitva7mishra/pookie-auth-cl1.git",
+        description: "Cute animated authentication UI",
+      },
+      grad_auth: {
+        name: "Gradient Auth",
+        repo: "https://github.com/Sarthak-Saghal/auth-cl1.git",
+        description: "Modern gradient auth",
+      },
+    },
+  },
+};
+
+export default components;
