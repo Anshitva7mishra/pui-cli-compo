424f87d8b446db945d08373ef17979fb389c8770
Anshitva7Mishra <mishraanshitva@gmail.com>
2026-01-13 02:53:49 +0530

    Hello

--- components.config.js
+++ components.config.js
@@ -10,22 +10,59 @@ const components = {
       },
     },
   },
+  navbar: {
+    label: "Navbar",
+    items: {
+      Food_Order_WebApp: {
+        key: "PUI_Foods_Navbar",
+        name: "PUI_Foods_Navbar",
+        repo: "https://github.com/Anshitva7mishra/PUI_CLI5_NAVBAR5.git",
+        description: "It's a navbar of food ordering webApp",
+      },
+      Appointment_WebApp: {
+        key: "Appointment_booking_Navbar",
+        name: "Appointment_booking_Navbar",
+        repo: "https://github.com/Anshitva7mishra/PUI_CLI4_NAVBAR4.git",
+        description: "It's a navbar of appointment booking ordering webApp",
+      },
+      PUI_Shop_WebApp: {
+        key: "PUI_Ecom_Navbar",
+        name: "PUI_Ecom_Navbar",
+        repo: "https://github.com/Anshitva7mishra/PUI_CLI1_NAVBAR1.git",
+        description: "It's a navbar of E-commerce webApp",
+      },
+      PUI_UI_Library_WebApp: {
+        key: "PUI_UI_Lib_Navbar",
+        name: "PUI_UI_Lib_Navbar",
+        repo: "https://github.com/Anshitva7mishra/PUI_CLI2_NAVBAR2.git",
+        description: "It's a navbar of UI-Library webApp",
+      },
+      LMS_WebApp: {
+        key: "PUI_LMS_Navbar",
+        name: "PUI_LMS_Navbar",
+        repo: "https://github.com/Anshitva7mishra/PUI_CLI1_NAVBAR1.git",
+        description: "It's a navbar of LMS webApp",
+      },
+    },
+  },
 
   auth: {
     label: "Auth",
     items: {
-       Pookie_Auth_Suite: {
+      Pookie_Auth_Suite: {
         key: "Pookie_Auth_Suite",
         name: "Pookie Auth Suite ",
         repo: "https://github.com/Anshitva7mishra/pookie-auth-cl1.git",
-        description: "A cute, playful authentication UI with smooth animations.",
+        description:
+          "A cute, playful authentication UI with smooth animations.",
       },
 
       Glassmorphism_Login_UI: {
         key: "Glassmorphism_Login_UI",
         name: "Glassmorphism Login UI",
         repo: "https://github.com/Sarthak-Saghal/auth-CLI_1.git",
-        description: "A beautiful glassmorphism login interface with soft gradients.",
+        description:
+          "A beautiful glassmorphism login interface with soft gradients.",
       },
     },
   },
