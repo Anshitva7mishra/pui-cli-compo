bf6f83c85fadb15b6b1e5de53132c9f509949232
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2026-01-25 18:23:05 +0530

    All components_CLI_completed

--- components.config.js
+++ components.config.js
@@ -77,7 +77,7 @@ const components = {
       DarkWave_Access_Portal: {
         key: "DarkWave_Access_Portal",
         name: "DarkWave_Access_Portal",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_1",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_1.git",
         description:
           "A cute, playful authentication UI with smooth animations.",
       },
@@ -85,7 +85,7 @@ const components = {
       CyberShield_Admin_Auth: {
         key: "CyberShield_Admin_Auth",
         name: "CyberShield_Admin_Auth",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_2",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_2.git",
         description:
           "Secure administrator access with enterprise-grade protection",
       },
@@ -104,7 +104,7 @@ const components = {
       Social_Login_FlipCoin: {
         key: "Social_Login_FlipCoin",
         name: "Social_Login_FlipCoin",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_3",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_3.git",
         description:
           "A cute, playful authentication UI with smooth animations.",
       },
@@ -112,7 +112,7 @@ const components = {
       Social_Continue_Form: {
         key: "Social_Continue_Form",
         name: "Social_Continue_Form",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_4",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_4.git",
         description:
           "A cute, playful authentication UI with smooth animations.",
       },
@@ -120,7 +120,7 @@ const components = {
        Sleek_SignIn_SignUp: {
         key: "Sleek_SignIn_SignUp",
         name: "Sleek_SignIn_SignUp",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_6",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_6.git",
         description:
           "A cute, playful authentication UI with smooth animations.",
       },
@@ -128,7 +128,7 @@ const components = {
       Auth_Slider: {
         key: "Auth_Slider",
         name: "Auth_Slider",
-        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_5",
+        repo: "https://github.com/Sarthak-Saghal/auth-CLI_A_5.git",
         description:
           "A cute, playful authentication UI with smooth animations.",
       },
@@ -158,6 +158,419 @@ const components = {
       },
     },
   },
+
+  auth: {
+    label: "Footer",
+    items: {
+
+
+      Animated_FooterUI: {
+        key: "Animated_FooterUI",
+        name: "Animated_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      MarketingPro_FooterUI: {
+        key: "MarketingPro_FooterUI",
+        name: "MarketingPro_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Business_FooterUI: {
+        key: "Business_FooterUI",
+        name: "Business_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Business_Hours_FooterUI: {
+        key: "Business_Hours_FooterUI",
+        name: "Business_Hours_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      DataStory_SaaS_FooterUI: {
+        key: "DataStory_SaaS_FooterUI",
+        name: "DataStory_SaaS_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Nicepay_Gradient_FooterUI: {
+        key: "Nicepay_Gradient_FooterUI",
+        name: "Nicepay_Gradient_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_6.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Agency_Connect_FooterUI: {
+        key: "Agency_Connect_FooterUI",
+        name: "Agency_Connect_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_7.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Hiring_CTA_FooterUI: {
+        key: "Hiring_CTA_FooterUI",
+        name: "Hiring_CTA_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_8.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Minimal_SaaS_FooterUI: {
+        key: "Minimal_SaaS_FooterUI",
+        name: "Minimal_SaaS_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_9.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Split_Gradient_FooterUI: {
+        key: "Split_Gradient_FooterUI",
+        name: "Split_Gradient_FooterUI",
+        repo: "https://github.com/Sarthak-Saghal/Footer_CLI_10.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+    }
+
+  },
+
+  sidebar: {
+    label: "Sidebar",
+    items: {
+
+
+      Sleek_Sidebar: {
+        key: "Sleek_Sidebar",
+        name: "Sleek_Sidebar",
+        repo: "https://github.com/Sarthak-Saghal/Sidebar_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Modern_Sidebar: {
+        key: "Modern_Sidebar",
+        name: "Modern_Sidebar",
+        repo: "https://github.com/Sarthak-Saghal/Sidebar_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      EduCore_Sidebar: {
+        key: "EduCore_Sidebar",
+        name: "EduCore_Sidebar",
+        repo: "https://github.com/Sarthak-Saghal/Sidebar_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      NeonLabs_Sidebar: {
+        key: "NeonLabs_Sidebar",
+        name: "NeonLabs_Sidebar",
+        repo: "https://github.com/Sarthak-Saghal/Sidebar_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Lumina_Systems_Sidebar: {
+        key: "Lumina_Systems_Sidebar",
+        name: "Lumina_Systems_Sidebar",
+        repo: "https://github.com/Sarthak-Saghal/Sidebar_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+
+
+    }
+
+  },
+
+
+  Loader: {
+    label: "loader",
+    items: {
+
+
+      Atomic_Orbiter: {
+        key: "Atomic_Orbiter",
+        name: "Atomic_Orbiter",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Molten_Infinity: {
+        key: "Molten_Infinity",
+        name: "Molten_Infinity",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Kinetic_Tesseract: {
+        key: "Kinetic_Tesseract",
+        name: "Kinetic_Tesseract",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Cyber_Glitch: {
+        key: "Cyber_Glitch",
+        name: "Cyber_Glitch",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Abyss_Reactor: {
+        key: "Abyss_Reactor",
+        name: "Abyss_Reactor",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+    }
+
+
+  },
+
+
+  Carousel: {
+    label: "carousel",
+    items: {
+
+
+      Perspective_CarouselUI: {
+        key: "Perspective_CarouselUI",
+        name: "Perspective_CarouselUI",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Astral_Drift_CarouselUI: {
+        key: "Astral_Drift_CarouselUI",
+        name: "Astral_Drift_CarouselUIAbyss_Reactor",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Cyber_Space_CarouselUI: {
+        key: "Cyber_Space_CarouselUI",
+        name: "Cyber_Space_CarouselUI",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+      Nightcrawler_CarouselUI: {
+        key: "Nightcrawler_CarouselUI",
+        name: "Nightcrawler_CarouselUI",
+        repo: "https://github.com/Sarthak-Saghal/Loader_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Solo_Leveling_CarouselUI: {
+        key: "Solo_Leveling_CarouselUI",
+        name: "Solo_Leveling_CarouselUI",
+        repo: "https://github.com/Sarthak-Saghal/Carousel_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+    }
+
+  },
+
+    Testinomial: {
+    label: "testinomial",
+    items: {
+
+      Testimonial_Carousel_UI: {
+        key: "Testimonial_Carousel_UI",
+        name: "Testimonial_Carousel_UI",
+        repo: "https://github.com/Sarthak-Saghal/Testinomial_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Dark_Stories_UI: {
+        key: "Dark_Stories_UI",
+        name: "Dark_Stories_UI",
+        repo: "https://github.com/Sarthak-Saghal/Testinomial_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Trusted_Leaders_UI: {
+        key: "Trusted_Leaders_UI",
+        name: "Trusted_Leaders_UI",
+        repo: "https://github.com/Sarthak-Saghal/Testinomial_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Light_Testimonial_UI: {
+        key: "Light_Testimonial_UI",
+        name: "Light_Testimonial_UI",
+        repo: "https://github.com/Sarthak-Saghal/Testinomial_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Success_Flow_Timeline_UI: {
+        key: "Success_Flow_Timeline_UI",
+        name: "Success_Flow_Timeline_UI",
+        repo: "https://github.com/Sarthak-Saghal/Testinomial_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+    }
+
+  },
+  
+  HeroSection: {
+    label: "herosection",
+    items: {
+
+      EduFlex_HeroSection: {
+        key: "EduFlex_HeroSection",
+        name: "EduFlex_HeroSection",
+        repo: "https://github.com/Sarthak-Saghal/HeroSection_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Bold_Fashion_HeroSection: {
+        key: "Bold_Fashion_HeroSection",
+        name: "Bold_Fashion_HeroSection",
+        repo: "https://github.com/Sarthak-Saghal/HeroSection_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Beauty_HeroSection: {
+        key: "Beauty_HeroSection",
+        name: "Beauty_HeroSection",
+        repo: "https://github.com/Sarthak-Saghal/HeroSection_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Watch_Grow_HeroSection: {
+        key: "Watch_Grow_HeroSection",
+        name: "Watch_Grow_HeroSection",
+        repo: "https://github.com/Sarthak-Saghal/HeroSection_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Clicks_Sales_HeroSection: {
+        key: "Clicks_Sales_HeroSection",
+        name: "Clicks_Sales_HeroSection",
+        repo: "https://github.com/Sarthak-Saghal/HeroSection_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+    },
+
+    
+
+  },
+
+  Card: {
+    label: "card",
+    items: {
+
+
+      Travel_Hiking_Card: {
+        key: "Travel_Hiking_Card",
+        name: "Travel_Hiking_Card",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_1.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Travel_Booking_Card: {
+        key: "Travel_Booking_Card",
+        name: "Travel_Booking_Card",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_2.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Dark_Fantasy_Lore_Card: {
+        key: "Dark_Fantasy_Lore_Card",
+        name: "Dark_Fantasy_Lore_Card",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_3.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Cyber_ProductCard_UI: {
+        key: "Cyber_ProductCard_UI",
+        name: "Cyber_ProductCard_UI",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_4.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Doctor_BookingCard_UI: {
+        key: "Doctor_BookingCard_UI",
+        name: "Doctor_BookingCard_UI",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_5.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Design_CourseCard_UI: {
+        key: "Design_CourseCard_UI",
+        name: "Design_CourseCard_UI",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_6.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+
+      Neural_SystemCard_UI: {
+        key: "Neural_SystemCard_UI",
+        name: "Neural_SystemCard_UI",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_7.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+      Web3_Social_OrbitCard_UI: {
+        key: "Web3_Social_OrbitCard_UI",
+        name: "Web3_Social_OrbitCard_UI",
+        repo: "https://github.com/Sarthak-Saghal/Card_CLI_8.git",
+        description: "A clean and modern multi-step signup interface.",
+      },
+
+
+    
+    }
+   
+  } 
+
+  
+
 };
 
 export default components;
